9134a83c2f2023e69e17127d806453cd263575eb
lucaspuig <puiglucas82@gmail.com>
2025-02-18 22:32:33 -0300

    arreglo

--- script.js
+++ script.js
@@ -228,22 +228,6 @@ document.querySelectorAll('.project-card1').forEach(card => {
     }
     });
 
-    //slide de servicios
-    const slidingContent = document.getElementById('slidingContent');
-    const categories = Array.from(slidingContent.children);
-    let currentIndex = 0;
 
-    function updateActiveCategory() {
-        categories.forEach(category => category.classList.remove('active'));
-        categories[currentIndex].classList.add('active');
-    }
-
-    function slideCategories() {
-        currentIndex = (currentIndex + 1) % categories.length;
-        slidingContent.style.transform = `translateX(-${currentIndex * 100}%)`;
-        updateActiveCategory();
-    }
-
-    setInterval(slideCategories, 5000);
 
 
