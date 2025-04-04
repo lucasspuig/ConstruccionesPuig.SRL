54fd5ef8404e38657bf7c6446888b9506737c86c
lucaspuig <puiglucas82@gmail.com>
2025-04-04 07:20:48 -0300

    arreglos

--- servicios/script.js
+++ servicios/script.js
@@ -1,3 +1,42 @@
+(function () {
+    const openButton = document.querySelector('.nav__menu'); // Botón para abrir el menú
+    const menu = document.querySelector('.nav__link--menu'); // Menú de navegación
+    const closeButton = document.querySelector('.nav__close'); // Botón para cerrar el menú
+    const menuLinks = document.querySelectorAll('.nav__link--menu a'); // Enlaces dentro del menú
+
+    if (openButton && menu) {
+        openButton.addEventListener('click', () => {
+            menu.classList.add('nav__link--show'); // Muestra el menú
+        });
+    }
+
+    if (closeButton && menu) {
+        closeButton.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show'); // Oculta el menú
+        });
+    }
+
+    // Cierra el menú al hacer clic en un enlace, excepto si es un dropdown
+    menuLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            if (!e.target.closest('.dropdown')) { 
+                menu.classList.remove('nav__link--show');
+            }
+        });
+    });
+
+    // Cierra el menú si se hace clic fuera de él
+    document.addEventListener('click', (e) => {
+        if (!menu.contains(e.target) && !openButton.contains(e.target)) {
+            menu.classList.remove('nav__link--show');
+        }
+    });
+
+})();
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let lastScrollY = window.scrollY;
     const nav = document.querySelector(".nav");
@@ -22,3 +61,5 @@ document.addEventListener("DOMContentLoaded", function () {
         lastScrollY = currentScrollY;
     });
 });
+
+
